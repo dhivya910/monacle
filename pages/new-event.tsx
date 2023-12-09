6a960456fad328284d5fbb2ce8fed801cc14ec74
Dhivya Dharshni S <dhivya.dharshni.s@sap.com>
2023-12-10 03:57:47 +0530

    added space img

---
+++ pages/new-event.tsx
@@ -0,0 +1,80 @@
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import * as React from "react";
+import Layout from "@/components/layout";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+
+export default function NewEvent() {
+  return (
+    <Layout pageTitle="">
+      <form>
+        <div className="space-y-12">
+          <div className="border-b border-white/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-white">
+              Create Event
+            </h2>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Title
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    autoComplete="given-title"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Description
+                </label>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DemoContainer components={["DateTimePicker"]}>
+                    <DateTimePicker label="Basic date time picker" />
+                  </DemoContainer>
+                </LocalizationProvider>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="description"
+                    id="description"
+                    autoComplete="description"
+                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-white"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </form>
+    </Layout>
+  );
+}
