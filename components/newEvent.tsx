31ac30a6a859125d2dace6b2f953c615c045ebe1
Dhivya Dharshni S <dhivya.dharshni.s@sap.com>
2023-12-10 03:42:06 +0530

    added spaces

--- components/newEvent.tsx
+++ components/newEvent.tsx
@@ -1,6 +1,7 @@
-import { PlusIcon } from '@heroicons/react/20/solid'
+import { PlusIcon } from "@heroicons/react/20/solid";
 
 export default function NewEvent() {
+
   return (
     <div className="text-center">
       <svg
@@ -19,16 +20,19 @@ export default function NewEvent() {
         />
       </svg>
       <h3 className="mt-2 text-sm font-semibold text-gray-900">No projects</h3>
-      <p className="mt-1 text-sm text-gray-500">Get started by creating a new project.</p>
+      <p className="mt-1 text-sm text-gray-500">
+        Get started by creating a new project.
+      </p>
       <div className="mt-6">
         <button
           type="button"
           className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          
         >
           <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
           New Project
         </button>
       </div>
     </div>
-  )
+  );
 }
